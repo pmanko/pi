6cab1a19a65050f7a2b9320fc9ccc502eb0c3d9c
Piotr Mankowski <pmankowski@partners.org>
2013-11-12 13:56:04 -0500

    route fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,30 @@
 Pi::Application.routes.draw do
   devise_for :users, :controllers => { registrations: 'auth/registrations'}
+
+  authenticated :user do
+    root :to => 'patient#profile', as: :user_root
+  end
+
+  unauthenticated do
+    as :user do
+      root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
+
+  root :to => "welcome#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root "patient#profile"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  get 'welcome/index'
+  get '/about' => 'welcome#about', as: :about
+
 
   get "users/:name/profile" => 'patient#profile', as: :patient_profile
 
--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,22 +2,38 @@ require "spec_helper"
 
 
 describe "Signs in user" do
+  before do
+    @password = "mypassword11"
+    @user = create(:confirmed_user, password: @password)
+
+    expect(@user.valid_password?(@password)).to be_true
+    expect(@user).to be_confirmed
+    expect(@user).to be_persisted
+  end
+
   it "should sign in a confirmed user using the main signin page", :js => true do
-    password = "mypassword11"
-    user = create(:confirmed_user, password: password)
-
-    #expect(User.confirm_by_token(user.confirmation_token)).to be_true
-
-    expect(user.valid_password?(password)).to be_true
-    expect(user).to be_confirmed
-    expect(user).to be_persisted
-
     visit new_user_session_path
+
+
+    fill_in("Email address", with: @user.email)
+    fill_in("Password", with: @password)
+
+    click_on "Sign in"
+
+    expect(User.count).to be > 0
+    expect(page).to have_content @user.full_name
+
+  end
+
+  it "should sign in a confirmed user using dropdown signin", :js => true do
+    visit root_path
+
+    click_link "user-menu-dropdown"
 
     page.save_screenshot("spec/screenshots/1.png")
 
-    fill_in("Email address", with: user.email)
-    fill_in("Password", with: password)
+    fill_in("Email address", with: @user.email)
+    fill_in("Password", with: @password)
 
     page.save_screenshot("spec/screenshots/2.png")
 
@@ -25,12 +41,9 @@ describe "Signs in user" do
 
     page.save_screenshot("spec/screenshots/3.png")
 
-
     expect(User.count).to be > 0
-    expect(page).to have_content user.full_name
-
+    expect(page).to have_content @user.full_name
 
 
   end
-
 end
